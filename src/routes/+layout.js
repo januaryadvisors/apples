5adfe897d4c09ef6754e7eab4848e94138b99c50
emikjackson <emij@umich.edu>
2023-04-26 20:55:49 -0700

    feat/add comment

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,4 @@
+// For deploying site as a SPA on GitHub Pages
+// https://medium.com/mkdir-awesome/a-piece-of-cake-deploying-sveltekit-on-github-pages-bc851ec860ad
 export const prerender = true;
 export const ssr = false;
